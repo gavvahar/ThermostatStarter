d9e4f34e8186fdbe8c23e0078a9b3cd3d715753f
gavvahar <nihargavva@gmail.com>
2022-05-09 12:07:09 -0400

    Get users temp

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// Note: We'll be using the Fahrenheit scale for this assignment!
-
-// This code sets a random starting current temperature between 0 and 100 degrees
 let currentTemp = Math.round(Math.random() * 100)
-
-// Give the user of the thermostat a status message
+let preferredTemp = 70;
+let usersPreferredTemp = prompt('Would you like to set a preferred temperature?\n(Type degrees in Fahrenheit or type "no".)')
+if(!isNaN(usersPreferredTemp))
+{
+    let numValue = Math.round(usersPreferredTemp);
+    if(numValue > 0 && numValue < 100)
+    {
+        preferredTemp = numValue;
+        console.log('Preferred temp set to ${preferredTemp}');
+    }else
+    {
+        console.log('The value you entered is invalid');
+    }
+}
 console.log(`The current temperature is ${currentTemp}°F`)
